81e40ef755c4794bec7c5d617411499c02129e17
Bright Adieme <bright.adieme@gmail.com>
2025-11-09 22:01:25 +0000

    Quick save 2025-11-09_22:01:25

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hello, world!");
+console.log("Hello, Bright! Welcome to javaScript");
+console.log("My name is bright");
+console.log("I love coding and building website!");
+console.log("JavaScript will help me make them interactive!");
+document.getElementById("greeting").innerText = "Hello  this is bright from JavaScript!"
+let name = "bright Adieme";
+console.log(name)
+
+/* mini practice */
+
+let myame = "Bright";
+let age = 25;
+let country = "Nigeria";
+console.log(`Hi, i,m ${myame} and i,m ${age}, i,m also from ${country}.`)
+
+let a = 5;
+let b = 10;
+let sum = a + b;
+console.log(`The sum of ${a} and ${b} is ${sum}.`)
+
+let knowsJavascript = true;
+console.log(`Do i know javascript ? ${knowsJavascript}`)
+
+document.getElementById("intro").innerText = `Hi, i,m ${myame} and i,m ${age}, i,m also from ${country}.`
+document.getElementById("math").innerText = `The sum of ${a} and ${b} is ${sum}.`
+document.getElementById("skill").innerText = `Do i know javascript ? ${knowsJavascript}`
